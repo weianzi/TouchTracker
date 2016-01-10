7a4693c1d8e3a6ad8fd8118c9ed2b40712de5ced
ancool <ancool@foxmail.com>
2016-01-10 15:53:16 +0800

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by an on 16/1/10.
+//  Copyright © 2016年 ancool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchTracker
+//
+//  Created by an on 16/1/10.
+//  Copyright © 2016年 ancool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
